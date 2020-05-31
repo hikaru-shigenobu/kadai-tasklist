f40280a11026997f3cd3d9c098270802db49c0f4
hikaru.shigenobu <shigenobu_0358@yahoo.co.jp>
2020-05-31 14:19:37 +0000

    fix for /migrate/20200519052241_add_references_to_tasks.rb

--- db/migrate/20200519052241_add_references_to_tasks.rb
+++ db/migrate/20200519052241_add_references_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddReferencesToTasks < ActiveRecord::Migration[5.2]
   def change
-    add_reference :tasks, :user, null: false, foreign_key: true
+    add_reference :tasks, :user, null: false, :default => 0, foreign_key: true
   end
 end
